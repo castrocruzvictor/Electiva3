1f004bc36e9a94f51f2900cd6b6746db42bce8fe
ESTUDIANTE <ESTUDIANTE@S4-19>
2017-08-16 06:36:32 -0500

    operaciones tercera clase

---
+++ Operaciones/src/Operacionesbits/bits.java
@@ -0,0 +1,32 @@
+package Operacionesbits;
+
+
+public class bits {
+    
+    public static void main(String[] args) {
+   
+        
+        //impresion de bits y movimientos 
+        int a =32;
+        int b;
+        
+        System.out.println(a);
+        b= a << 2;
+        System.out.println(b);
+        b= a >> 2;
+        System.out.println(b);
+        b= a & 2;
+        System.out.println(b);
+        
+        //ciclo for 
+        int i;
+        for(i=0; i<5; i++ ){
+           System.out.println(i);
+        }
+        
+        
+        
+        
+    }
+    
+}

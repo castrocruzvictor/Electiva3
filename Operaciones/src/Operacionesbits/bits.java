9b0f10353160a282fbb9b0340be53ebaeb67a0aa
ESTUDIANTE <ESTUDIANTE@S4-19>
2017-08-16 06:58:31 -0500

    operaciones tercera clase

--- Operaciones/src/Operacionesbits/bits.java
+++ Operaciones/src/Operacionesbits/bits.java
@@ -18,7 +18,7 @@ public class bits {
         b= a & 2;
         System.out.println(b);
         
-        //ciclo for 
+        //ciclo for cero
         int i;
         for(i=0; i<5; i++ ){
            System.out.println(i);
@@ -27,6 +27,50 @@ public class bits {
         
         System.out.println(i);
         
+        //ciclo for para saltar numero
+        for(i=0; i<5; i++ ){
+           if (i==3)
+               continue;
+           System.out.println(i);
+        }
+        
+        System.out.println(i);
+        
+        
+        //while para subir de dos en dos pregunta antes de hacer
+        i=2;
+        while (i<10){
+            System.out.println(i);
+            i+= 2;
+        }
+        
+        //el do while hace y despues pregunta 
+        //do{
+        //    System.out.println(i);
+        //    i+= 2;
+            
+        //}while(i<10);
+        
+        if(i<=8){
+            System.out.println("menor igual que 8");
+        }
+        else if (i > 10){
+            System.out.println("mayor a 10");
+        }
+        else {
+            System.out.println("entre 9 y 10");
+        }
+        
+        switch(i){
+            case 8:
+                System.out.println("igual que 8");
+            case 10:
+                System.out.println("igual que 10");
+                break;            
+            default:
+                System.out.println("diferente");
+                
+        }
         
     }
     
